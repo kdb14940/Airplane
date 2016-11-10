708db0a45c6c1e2c94a7ac2a753078d2f50a9fad
addcninblue <addison95132@gmail.com>
2016-11-09 16:00:08 -0800

    off by one error

--- AirlineCompany.java
+++ AirlineCompany.java
@@ -379,7 +379,7 @@ public class AirlineCompany {
                 if(temp.getFirstName().equalsIgnoreCase(firstName) && temp.getLastName().equalsIgnoreCase(lastName)){
                     int column = temp.getColumn();
                     int row = temp.getRow();
-                    airplane.getAirplaneSeats()[column-1][row-1].isVacant = true;
+                    airplane.getAirplaneSeats()[column][row].isVacant = true;
                     passengers.remove(i);// removes passenger from the list
                     System.out.println("Your seat has been successfully canceled");
                     System.out.println();
